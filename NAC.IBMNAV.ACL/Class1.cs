b63a59a5bcd68967139df415fa771ffa83119045
Charles Pretorius <charles@LANSERIA.NAC.CO.ZA>
2018-05-15 13:14:25 +0200

    Add project files.

---
+++ NAC.IBMNAV.ACL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAC.IBMNAV.ACL
+{
+    public class Class1
+    {
+    }
+}
